779373219c0c9b81539f7387bf9f4bac26a21579
Dima <dimadima9696@gmail.com>
2023-01-30 22:35:19 +0200

    test commit

--- src/features/vehicles/Vehicles.tsx
+++ src/features/vehicles/Vehicles.tsx
@@ -2,7 +2,7 @@ import { useGetVehiclesQuery } from 'data/vehicles';
 import * as React from 'react';
 
 export const Vehicles: React.FC = () => {
-  const { data } = useGetVehiclesQuery();
+  // const { data } = useGetVehiclesQuery();
 
   return <div>Vehicles</div>;
 };
